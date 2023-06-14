b3c772abb2c95799c2a76fcfb8ec03a4b65b9e58
UltraHertzz <li15210382115@hotmail.com>
2023-06-14 17:47:18 +0200

    imreader func complete

--- imreader.m
+++ imreader.m
@@ -1,35 +1,22 @@
-function image = imreader(dir,extension)
+function imageDataList = imreader(direc)
 % IMREADER 
-%   读取图片数据并另存为.mat格式(load RGBD data and saving as (*.mat) format)
-%   输入I  ：图片文件所在地址
-%   输入II ：图片扩展名
+%   读取图片数据，返回字典imageDataList
+%   输入I  ：图片文件所在地址 path of the pictures,
+%           字典对应i-th图片imageDataList{i}内存有像素+rgb-channel
     
-    % Define the folder containing the images 
-    imageFolder = 'dir';
-
-    % Get a list of all image files in the folder
-    imageFiles = dir(fullfile(imageFolder,'*.%s', extension));
+    imageFiles = dir(fullfile(direc,'*.JPG'));
+    
+    % Init array to store image data
+    imageDataList = {};
 
     % load image file recursively
     for i = 1:numel(imageFiles)
         % Read the image
-        imagePath = fullfile(imageFolder, imageFiles(i).name);
+        imagePath = fullfile(direc, imageFiles(i).name);
         image = imread(imagePath);
 
-        % Perform image processing or feature detection if needed
-
-        % Store 3D point and RGB value data in a structure
-        pointCloudData = struct();
-        pointCloudData.Points = [];
-        pointCloudData.RGB = [];
-
-        % Save the point cloud data to a .mat file
-        [~, imageName, ~] = fileparts(imagePath);
-        savePath = fullfile(imageFolder,[imageName, '.mat']);
-        save(savePath, 'pointCloudData');
-
-        % Display progress
-        fprintf('Processed image %d of %d: %s\n', i ,numel(imageFiles), imageFiles(i).name);
+        % Append the image data to the list
+        imageDataList{i} = image;
     end
    
 end
